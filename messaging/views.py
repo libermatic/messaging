042ddf47588c1f859c3f4ad4871c29c30aa37859
Sun Howwrongbum <sun@libermatic.com>
2018-08-04 04:08:55 +0530

    Add routes for /accounts

---
+++ messaging/views.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+from flask import request, jsonify, abort
+from google.appengine.api.datastore_errors import (
+    BadValueError, BadRequestError
+)
+import logging
+from functools import partial
+from toolz import compose
+
+from messaging import app, accounts
+
+
+@app.route('/accounts', methods=['GET', 'POST'])
+@app.route('/accounts/<id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_accounts(id=None):
+    if id:
+        try:
+            if request.method == 'GET':
+                return compose(jsonify, accounts.get)(id)
+            if request.method == 'PUT':
+                return compose(
+                    jsonify, partial(accounts.update, id), request.get_json
+                )()
+            if request.method == 'DELETE':
+                return compose(jsonify, accounts.delete)(id), 204
+        except ReferenceError as e:
+            logging.exception(e)
+            return abort(404, 'Entity not found')
+    if request.method == 'GET':
+        return compose(jsonify, accounts.list)()
+    if request.method == 'POST':
+        try:
+            return compose(jsonify, accounts.create, request.get_json)()
+        except (BadValueError, BadRequestError) as e:
+            logging.exception(e)
+            return abort(400, 'Invalid datatype or missing required fields')
+        except ReferenceError as e:
+            logging.exception(e)
+            return abort(400, 'Entity already exists')
+    return abort(400, 'WTF')
+
+
+@app.route('/')
+def index():
+    return 'Nada'
+
+
+@app.errorhandler(400)
+def handle_bad_request(error):
+    return jsonify({'error': error.description}), 400
+
+
+@app.errorhandler(404)
+def handle_not_found(error):
+    return jsonify({'error': error.description}), 404
+
+
+@app.errorhandler(405)
+def handle_method_not_allowed(error):
+    return jsonify({'error': error.description}), 405

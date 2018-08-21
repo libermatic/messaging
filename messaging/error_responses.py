7788270d11f997866c2cf5dd0617a85481a9f829
Sun Howwrongbum <sun@libermatic.com>
2018-08-21 17:30:27 +0530

    Add error response

--- messaging/error_responses.py
+++ messaging/error_responses.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 NOT_FOUND = 'Entity not found'
+REF_NOT_FOUND = 'Referenced entity not found'
 INVALID_FIELD = 'Invalid datatype or missing required fields'
 ALREADY_EXISTS = 'Entity already exists'
